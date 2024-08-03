0a36f8929a1264808f1501de30f15d4f329f1841
Gilberto Junior <giljrsantos@gmail.com>
2024-08-03 20:21:12 -0300

    feat: validation of body, query, params and headers with YUP and express

--- src/server/controllers/redes/create.ts
+++ src/server/controllers/redes/create.ts
@@ -1,11 +1,39 @@
 import { Request, Response } from 'express';
 import { IRedes } from '../../../interface/i-Redes';
+import * as yup from 'yup';
 
-export const create = (
+const bodyValidation: yup.SchemaOf<IRedes> = yup.object().shape({
+    id_master: yup.number().required(),
+    id_coord_grupo: yup.number().required(),
+    nome_rede: yup.string().required().min(3),
+    contrato_Qtde: yup.number().required(),
+    contrato_valor: yup.number().required(),
+    contrato_valor_adicional: yup.number().required(),
+    status_rede: yup.number().required()
+});
+
+export const create = async (
   req: Request<{}, {}, IRedes>,
   res: Response,
 ) => {
-  console.log(req.body);
 
-  return res.send(req.body);
+    let validateData: IRedes | undefined = undefined;
+
+    try{
+        validateData = await bodyValidation.validate(req.body);
+    }catch(error){
+        const yupError = error as yup.ValidationError;
+        console.log(`Error ${yupError}`);
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        });
+    };
+
+    console.log(validateData);
+  
+
+  return res.send(validateData);
 };
